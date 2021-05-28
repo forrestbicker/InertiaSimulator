f27162e037d4793298cac449dceab59def410b09
Forrest Bicker <bickerforrest@gmail.com>
2021-05-27 21:10:01 -0400

    add method to step body foward in time dt secconds

--- src/Body.ts
+++ src/Body.ts
@@ -1,9 +1,16 @@
+import { config } from "mathjs";
+import { Config } from "./Config";
+import { vect } from "./Math/Util";
 import { Vector } from "./Math/Vector";
+import { VectorFunction } from "./Math/VectorFunction";
+
 export class Body {
 	public name: string;
 	public r: Vector;
 	public ri: Vector;
 	public v: Vector;
+	private time: number;
+
 	public forceArr: VectorFunction[];
 	public color: string;
 
@@ -26,6 +33,23 @@ export class Body {
 	public removeForce(force: VectorFunction): void {
 		let ix: number = this.forceArr.findIndex((element) => element == force);
 		this.forceArr.splice(ix);
+	}
+
+	public step(dt: number): void {
+		// a number of strange calculations must be added in order to convert the axies from HTML5 canvas's axies to our desired axies. In HTML, the origin is located at the top left corner, right is the positive x direction, and down is the positive y direction. We want the origin located at the bottom left corcner, right to be the positive x direction, and up to be the positive y direction. To accomplish this, we offset all y positions by the height of the canvas, and flip the sign of all y vector by subtracting a*dt and v*dt instead of adding them as intuition would suggest
+
+		// solve dx and dy
+		this.r.x += this.v.x * dt;
+		this.r.y += this.v.y * dt;
+
+		// sums up forces to find net acceleration and solve for dv
+		for (var i = 0; i < this.forceArr.length; i++) {
+			let a = this.forceArr[i].at(this, dt); // f = ma where m = 1
+			this.v.x -= a.x * dt;
+			this.v.y -= a.y * dt;
+		}
+
+		this.time += dt;
 	}
 	}
 }
