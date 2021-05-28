b94349ff1d4ee5c1121e758de30988dcdccfa438
Forrest Bicker <bickerforrest@gmail.com>
2021-05-27 21:06:18 -0400

    add Object-based tracking of forces on each Body

--- src/Body.ts
+++ src/Body.ts
@@ -4,6 +4,7 @@ export class Body {
 	public r: Vector;
 	public ri: Vector;
 	public v: Vector;
+	public forceArr: VectorFunction[];
 	public color: string;
 
 	constructor(name: string, ri: Vector, vi: Vector) {
@@ -11,8 +12,20 @@ export class Body {
 		this.color = "#000000";
 
 		this.ri = new Vector(ri.x, ri.y); // copy object so changing r does not impact stored version of r initial
+		this.forceArr = [];
 
 		this.r = ri;
 		this.v = vi;
+		this.time = 0;
+	}
+
+	public addForce(force: VectorFunction): void {
+		this.forceArr.push(force);
+	}
+
+	public removeForce(force: VectorFunction): void {
+		let ix: number = this.forceArr.findIndex((element) => element == force);
+		this.forceArr.splice(ix);
+	}
 	}
 }
