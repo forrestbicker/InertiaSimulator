0c91eb3ae2697b56da54bd9a67cfc3a63255da61
Forrest Bicker <bickerforrest@gmail.com>
2021-05-29 22:30:28 -0400

    create HTMLVector as DynamicVector that pulls equations from HTML input

---
+++ src/Math/HTMLVector.ts
@@ -0,0 +1,35 @@
+import { Body } from "../Body";
+import { Vector } from "./Vector";
+import { parse } from "mathjs";
+import { DynamicVector } from "./DynamicVector";
+
+/**
+ * A vector defined as a function that varies where the function is given by a html input field
+ */
+export class HTMLVector implements DynamicVector {
+	public xField: HTMLInputElement;
+	public yField: HTMLInputElement;
+
+	constructor(name: HTMLInputElement, xField: HTMLInputElement, yField: HTMLInputElement) {
+		// stores location of user input field
+		this.xField = xField;
+		this.yField = yField;
+	}
+
+	/** evaluates the function for a given body's position and velocity at a given time
+	 */
+	at(body: Body, time: number): Vector {
+		// parses out the equation in the input field
+		let xEquation = parse(this.xField.value);
+		let yEquation = parse(this.xField.value);
+
+		let mapping: Record<string, number> = { // mapping dict defines variables
+			x: body.r.x,
+			y: body.r.y,
+			vx: body.v.x,
+			yx: body.v.y,
+			t: time
+		}
+		return new Vector(xEquation.evaluate(mapping), yEquation.evaluate(mapping));
+	}
+}
