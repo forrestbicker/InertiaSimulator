fab8de003f4ef375cfd55b5081b72c57674f2d8a
Forrest Bicker <bickerforrest@gmail.com>
2021-05-29 22:31:56 -0400

    add Universe methods to draw state progression over time

--- src/Universe.ts
+++ src/Universe.ts
@@ -18,4 +18,30 @@ export class Universe {
 		this.nonInertialContext = nonInertialContext;
 
 	}
+	/** advance the universe foward in time by interval dt using instantaneous v and a to compute dx and dv*/
+	private step(dt: number): void {
+		for (var i = 0; i < this.bodies.length; i++) {
+			this.bodies[i].step(dt);
+		}
+		this.referenceFrame.step(dt);
+	}
+
+	private draw(): void {
+		this.inertialContext.clearRect(0, 0, Config.canvasWidth, Config.canvasHeight);
+		this.nonInertialContext.clearRect(0, 0, Config.canvasWidth, Config.canvasHeight);
+
+		this.referenceFrame.drawInertial(this.inertialContext);
+		this.referenceFrame.drawNonInertial(this.nonInertialContext, this.referenceFrame);
+		for (var i = 0; i < this.bodies.length; i++) {
+			this.bodies[i].drawInertial(this.inertialContext);
+			this.bodies[i].drawNonInertial(this.nonInertialContext, this.referenceFrame);
+		}
+	}
+
+	public run(): void {
+		setInterval(() => {
+			this.step(1 / Math.pow(2, 5));
+			this.draw();
+		}, Math.pow(2, 5))
+	}
 }
